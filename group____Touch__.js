be29d81dbbf35e964095b83f8f085c7278e2f877
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-07 16:43:31 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ group____Touch__.js
@@ -0,0 +1,5 @@
+var group____Touch__ =
+[
+    [ "gslc_GetTouch", "group____Touch__.html#ga0200233a29a18cc9882734d083d8b792", null ],
+    [ "gslc_InitTouch", "group____Touch__.html#ga46998dcdf5922171c36cfd8ca0f08526", null ]
+];
---
+++ structgslc__tsGui.js
@@ -0,0 +1,28 @@
+var structgslc__tsGui =
+[
+    [ "asFont", "group____Int__.html#ga78c133344855170060c3db9d9fe9699e", null ],
+    [ "asInputMap", "group____Int__.html#gadf5f545cdf3cae2556f0fdf473d4c148", null ],
+    [ "asPage", "group____Int__.html#ga44af0b3ef3a0d06e70fd01e04877351c", null ],
+    [ "bRedrawPartialEn", "group____Int__.html#ga2d2e7104967ed372a2cee5164c3f2706", null ],
+    [ "nDispDepth", "group____Int__.html#gac9cfaaaad375f0226b98c9e659b0f570", null ],
+    [ "nDispH", "group____Int__.html#gad608d97f5942460a7de20d386fb75a5d", null ],
+    [ "nDispW", "group____Int__.html#ga526017d1503465bf204e545782954373", null ],
+    [ "nFontCnt", "group____Int__.html#gae5a5172cefcd834e7baf0ff5579fcd5a", null ],
+    [ "nFontMax", "group____Int__.html#gac758d0abbc79f110fc6efb78e6fef4c7", null ],
+    [ "nFrameRateCnt", "group____Int__.html#ga45fc141b140e65edca93cbb6ff64cb10", null ],
+    [ "nFrameRateStart", "group____Int__.html#gab112c20c1939b8ab993efdd6906f91cd", null ],
+    [ "nInputMapCnt", "group____Int__.html#ga6ae1ed8ecbcfc4792af630a714fd7bf6", null ],
+    [ "nInputMapMax", "group____Int__.html#gaa7bcb2ef7b8c4d9c930f6904d8d16178", null ],
+    [ "nPageCnt", "group____Int__.html#ga7d2e138a9ee3576165a129a01db8d66a", null ],
+    [ "nPageMax", "group____Int__.html#gafb8a9b82f2a2cc11be1f4851c9058131", null ],
+    [ "nTouchLastPress", "group____Int__.html#ga8ce8e7d39004995b51f851e8e17c998b", null ],
+    [ "nTouchLastX", "group____Int__.html#ga9a86fa51b586f73da194d76d40e258a9", null ],
+    [ "nTouchLastY", "group____Int__.html#ga1da48204c2f183c65c2ed5896c3352c0", null ],
+    [ "pCurPage", "group____Int__.html#ga9d17c57a2dd9cc1d0b56b8e75eb230b5", null ],
+    [ "pCurPageCollect", "group____Int__.html#ga0520c539186e6872415b6e4f76547c4e", null ],
+    [ "pfuncPinPoll", "group____Int__.html#ga996a557aed81252e487a93b192f76fc9", null ],
+    [ "pfuncXEvent", "group____Int__.html#gad92a76dd79c9f99709c69c81154cac58", null ],
+    [ "pvDriver", "group____Int__.html#ga8f03fd974a270104a39a03a92d69bb2e", null ],
+    [ "sElemTmpProg", "group____Int__.html#ga95db3176631d94437e8c2c2f3de68033", null ],
+    [ "sImgRefBkgnd", "group____Int__.html#ga91ac1b7aa601bb217ae3a67340b82325", null ]
+];
